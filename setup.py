8795b3b4b893d1753c45128bd62969234f9aefd8
Laura Abro <laura@koii.network>
2025-04-24 13:56:14 -0300

    publish new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prometheus-swarm",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         # We'll get these from requirements.txt
